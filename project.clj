54046890de0c491384fac356413e000a8323bcf9
Anthony Cagle (on Linux) <anthony@vertegal.com>
2014-07-23 12:58:28 -0700

    switch namespace to secret-shame

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject aa-atw-om "0.1.0"
+(defproject secret-shame "0.1.0"
   :description "FIXME: write this!"
   :url "http://example.com/FIXME"
   :dependencies [[org.clojure/clojure "1.6.0"]
@@ -20,10 +20,10 @@
   :aliases {"up" ["pdo" "cljsbuild" "auto" "dev," "ring" "server-headless"]}
 
   :min-lein-version "2.0.0"
-  :uberjar-name "aa-atw-om-standalone.jar"
+  :uberjar-name "secret-shame-standalone.jar"
 
-  :ring {:handler aa-atw-om.core/app
-         :init aa-atw-om.core/init}
+  :ring {:handler secret-shame.core/app
+         :init secret-shame.core/init}
 
   :source-paths ["src/clj"]
 
@@ -34,29 +34,24 @@
                    :dependencies [[ring-mock "0.1.5"]
                                   [com.cemerick/double-check "0.5.7"]
                                   [com.cemerick/piggieback "0.1.3"]]
-                   :repl-options {:init-ns aa-atw-om.core
+                   :repl-options {:init-ns secret-shame.core
                                   :nrepl-middleware [cemerick.piggieback/wrap-cljs-repl]}
                    :source-paths ["dev"]
-                   :cljsbuild {:builds [{:id :dev
-                                         :source-paths ["src/clj" "src/cljs"]
-                                         :compiler
-                                         {:output-to "resources/public/js/main.js"
-                                          :output-dir "resources/public/js/out"
-                                          :optimizations :none
-                                          :libs [""]
-                                          :source-map true
-                                          :pretty-print true}}]}}}
-  
-  :cljsbuild {:builds [{:id "dev"
-                        :source-paths ["src/cljs"]
-                        :compiler {:output-to "resources/public/js/app.js"
-                                   :output-dir "resources/public/js/out"
-                                   :optimizations :none
-                                   :source-map true}}
-                       {:id "release"
-                        :source-paths ["src/cljs"]
-                        :compiler {:output-to "resources/public/js/app.js"
-                                   :optimizations :advanced
-                                   :pretty-print false
-                                   :preamble ["react/react.min.js"]
-                                   :externs ["react/externs/react.js"]}}]})
+                   :cljsbuild {:builds
+                               [{:builds nil
+                                 :source-paths ["src/cljs"]
+                                 :compiler {:pretty-print true
+                                            :output-to "resources/public/js/app.js"
+                                            :output-dir "resources/public/js/out"
+                                            :libs [""]
+                                            :optimizations :none
+                                            :source-map true}}]}}
+             :prod {:cljsbuild
+                    {:builds
+                     [{:builds nil
+                       :source-paths ["src/cljs"]
+                       :compiler {:output-to "resources/public/js/app.js"
+                                  :optimizations :advanced
+                                  :pretty-print false
+                                  :preamble ["react/react.min.js"]
+                                  :externs ["react/externs/react.js"]}}]}}})
--- resources/public/js/out/.js
+++
@@ -1,3 +0,0 @@
-// Compiled by ClojureScript 0.0-2268
-
-//# sourceMappingURL=.js.map
